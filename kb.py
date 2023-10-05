3fb4b769b8ac377e3dcce1f3241407656a60e838
Shpindik <Kuisajke@gmail.com>
2023-10-05 19:47:00 +0300

    Tg

---
+++ kb.py
@@ -0,0 +1,8 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+menu = [
+    [InlineKeyboardButton(text="📝 Запись", callback_data="record"),
+    InlineKeyboardButton(text="🔎 Информация", callback_data="information")]
+]
+menu = InlineKeyboardMarkup(inline_keyboard=menu)
+exit_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="◀️ Выйти в меню")]], resize_keyboard=True)
+iexit_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="◀️ Выйти в меню", callback_data="menu")]])
